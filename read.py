04632123db63736297afc5f8ffea1b05796b4310
Elaine Wang <elaine35002@gmail.com>
2021-10-02 18:31:55 +0800

    calc avg length

--- read.py
+++ read.py
@@ -7,4 +7,9 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 
+print('總共有', len(data), '筆資料')
 
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('平均長度是', sum_len/len(data))
