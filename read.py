a1fc223f983336e87a41d65ea7771da973fe6888
Elaine Wang <elaine35002@gmail.com>
2023-03-02 15:08:03 +0800

    add counting func

--- read.py
+++ read.py
@@ -12,33 +12,34 @@ print('總共有', len(data), '筆資料')
 print(data[0])
 
 # sum_len = 0
-# for d in data:
-# 	sum_len = sum_len + len(d)
-# print('平均長度是', sum_len/len(data))
+for d in data:
+	sum_len = sum_len + len(d)
+print('平均長度是', sum_len/len(data))
 
 
-# # 篩選 小於一百字的留言
+# 篩選 小於一百字的留言
 
-# new = []
-# for d in data:
-# 	if len(d) < 100:
-# 		new.append(d)
-# print('一共有', len(new), '筆留言長度<100')
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度<100')
 
-# # 篩選有提到good的留言
+# 篩選有提到good的留言
 
-# good = []
-# for d in data:
-# 	if 'good' in d:
-# 		good.append(d)	
-# print('一共有', len(good), '筆留言提到good')
-# print(good[0])
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)	
+print('一共有', len(good), '筆留言提到good')
+print(good[0])
 
-# # 另一種篩選快寫法
-# good = [d for d in data if 'good' in d]
-# print('一共有', len(good), '筆留言提到good')
+# 另一種篩選快寫法
+good = [d for d in data if 'good' in d]
+print('一共有', len(good), '筆留言提到good')
 
 
+#文字計數
 wc = {} #word count
 for d in data:
 	words = d.split()
@@ -57,4 +58,7 @@ print(wc['Elaine'])
 
 while True:
 	word = input('請問你想查什麼字?')
+	if word == 'q':
+		break
 	print(word, '出現過的次數為', wc[word])
+print('感謝使用本查詢功能')
